8dd4db6aead00a1f329929693b8e029ff82b82e1
Dverybest <duio1541@gmail.com>
2024-03-05 22:35:08 -0500

    tried my first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("Hello upwork")
+const abtBtn = document.querySelector("#abtBtn")
+
+abtBtn.addEventListener('click', function(){
+    window.location.href = '/about.html'})
+
+    const hmbtn = document.querySelector("#hmbtn")
+hmbtn.addEventListener('click', function (){
+    window.location.href = './index.html'
+})
+const fqbtn = document.querySelector('#fqbtn')
+fqbtn.addEventListener('click', function(){
+    window.location.href = './faq.html'})
